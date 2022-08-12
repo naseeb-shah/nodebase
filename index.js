2929064f50158035ba09a8ee149b0a0db057cc9a
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-08-12 11:09:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+ var process= require('process')
+ var {add, sub,multi,divid}=require('./calcutere')
+  
+
+var cmd=process.argv
+
+if(cmd[2]=='add'){
+    // console.log(typeof(cmd[3]))
+    console.log(add(+cmd[3],+cmd[4]))
+}
+
+
+if(cmd[2]=='sub'){
+    // console.log(typeof(cmd[3]))
+    console.log(sub(+cmd[3],+cmd[4]))
+}
+if(cmd[2]=='multi'){
+    // console.log(typeof(cmd[3]))
+    console.log(multi(cmd[3],+cmd[4]))
+}
+if(cmd[2]=='divid'){
+    // console.log(typeof(cmd[3]))
+    console.log(divid(+cmd[3],+cmd[4]))
+}
+
+
+
+
